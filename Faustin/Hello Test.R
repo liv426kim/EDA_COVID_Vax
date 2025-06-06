1eddf2b8a5165c469cb23d6981dc696187d62b6b
Frostyfausty <jordan.faustin@bison.howard.edu>
2025-06-06 14:37:01 -0400

    Update Hello Test.R

--- Faustin/Hello Test.R
+++ Faustin/Hello Test.R
@@ -34,12 +34,28 @@ ccd_prop <- ccd |>
 ggplot(ccd_prop, aes(x = fct_rev(factor(county)), y = proportion, fill = vaccine)) +
   geom_col(position = "stack") +
   coord_flip() +  # Flip coordinates for horizontal bars
-  theme_light() +
+  scale_y_continuous(
+    limits = c(0, 1),
+    breaks = seq(0, 1, by = 0.2),
+    labels = scales:: percent_format(accuracy = 1),
+    expand = expansion(mult = c(0, 0.02))) +
+  theme_minimal(base_size = 12) +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line.x = element_line(color = "gray50"),
+    axis.ticks.x = element_line(color = "gray50"),
+    plot.title = element_text(face = "bold", size = 14),
+    legend.position = "top",
+    legend.title = element_text(face = "bold"),
+    plot.margin = margin(1, 1, 1, 1, "cm")
+  ) +
   labs(
-    title = "Proportion of Vaccine Doses Shipped by County",
-    x = "County",
+    title = "Proportion of Vaccine Doses by County (Alphabetical Order)",
+    x = NULL,
     y = "Proportion of Total Doses",
-    fill = "Vaccine Type") +
+    fill = "Vaccine Manufacturer"
+  )+
   scale_fill_manual(values = c("pfizer" = "darkblue", 
                                "moderna" = "firebrick", 
-                               "jj" = "#e31a1c")) 
+                               "jj" = "#FF9999"))
