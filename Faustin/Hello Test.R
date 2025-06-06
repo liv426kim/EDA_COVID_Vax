7cd21a4d7769511830f10ac252f04794616d97e6
Jordan Faustin <jordan.faustin@bison.howard.edu>
2025-06-06 13:41:10 -0400

    second

--- Faustin/Hello Test.R
+++ Faustin/Hello Test.R
@@ -1,1 +1,15 @@
-Hello World
+install.packages("tidyverse")
+library(tidyverse)
+covid_cases_deaths <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/covid_california.csv")
+
+ccd <- covid_cases_deaths
+
+names(ccd)
+table(ccd$county)
+
+# x= week
+# y= cumulative_doses_shipped
+
+
+ccd |>
+  group_by(county, )
