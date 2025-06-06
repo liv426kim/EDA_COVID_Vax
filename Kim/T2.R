576f9966e0ef51380ca29205d7047f7251451229
liv426kim <liv426kim@gmail.com>
2025-06-06 14:28:44 -0400

    Update T2.R

--- Kim/T2.R
+++ Kim/T2.R
@@ -1,5 +1,6 @@
 Olivia Kim File
 
+
 library(tidyverse)
 covid_cases_deaths <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/covid_california.csv")
 
@@ -33,3 +34,53 @@ ccd |>
     x = "Week",
     y = "Shipping Rate (Delivered / Shipped)") +
   theme_minimal()
+
+ccd |>
+  group_by(county) |>
+  summarize(Pfizer = sum(pfizer_doses_delivered, na.rm = TRUE),
+    Moderna = sum(moderna_doses_delivered, na.rm = TRUE),
+    Johnson_Johnson = sum(jj_doses_delivered, na.rm = TRUE)) |>
+  pivot_longer(cols = c(Pfizer, Moderna, Johnson_Johnson),
+               names_to = "Brand",
+               values_to = "Doses_Delivered")|>
+  ggplot(aes(x = reorder(county, -Doses_Delivered), y = Doses_Delivered, fill = Brand)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = "Total COVID-19 Vaccine Doses Delivered by County and Brand",
+    x = "County",y = "Doses Delivered") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_brewer()
+
+#calc proportions by county
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+ccd_prop <- ccd |>
+  group_by(county) |>
+  summarise(
+    pfizer = sum(pfizer_doses_shipped, na.rm = TRUE),
+    moderna = sum(moderna_doses_shipped, na.rm = TRUE),
+    jj = sum(jj_doses_shipped, na.rm = TRUE)) |>
+  mutate(total = pfizer + moderna + jj) |>
+  mutate(pfizer = pfizer / total,
+    moderna = moderna / total,
+    jj = jj / total) |>
+  select(-total) |>
+  pivot_longer(cols = c(pfizer, moderna, jj),
+               names_to = "vaccine",
+               values_to = "proportion")
+
+ggplot(ccd_prop, aes(x = fct_rev(factor(county)), y = proportion, fill = vaccine)) +
+   geom_col(position = "stack") +
+  scale_fill_manual(values = c("pfizer" = "darkblue", 
+                               "moderna" = "firebrick", 
+                               "jj" = "#FF9999"))  +
+   coord_flip() +
+   theme_minimal() +
+   labs(title = "Proportion of Vaccine Doses Shipped by County",
+     x = "County",
+     y = "Proportion of Total Doses",
+     fill = "Vaccine Type")
+
